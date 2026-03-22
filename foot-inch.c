f1713d9e6336766d3a6d77e848ca8e4d7b610694
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-03-22 22:15:55 +0530

    Add files via upload

---
+++ foot-inch.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float  foot,inches;
+    printf("Enter a number in foot:");
+    scanf("%f",&foot);
+    inches=foot*12;
+    printf("inches=%f\n",inches);
+    return 0;
+}
