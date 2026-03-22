aa1cc4c3a2aea068c2828a379effe46d946be007
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-03-22 23:11:14 +0530

    Add files via upload

--- Mile to km.c
+++ Mile to km.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter a number in mile:");
     scanf("%f",&mile);
       km= mile * 1.609 ;
-      printf("Distance in kilometers=%f\n",km);
+      printf("Distance in kilometers=%.3f\n",km);
       return 0;
     
 }
