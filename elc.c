7f5992c5a114cb5a9d4664009887f718bba048b5
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-03-30 23:17:02 +0530

    Add files via upload

--- elc.c
+++ elc.c
@@ -14,10 +14,12 @@ int main() {
         printf("Enter Units Consumed: ");
         scanf("%d", &units);
 
-        if (units <= 100) {
+        if (units <= 100) 
+        {
             bill = units * 5;
         } 
-        else if (units <= 200) {
+        else if (units <= 200) 
+        {
             bill = (100 * 5) + ((units - 100) * 7);
         } 
         else {
