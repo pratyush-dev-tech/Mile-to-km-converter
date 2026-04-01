649b98090659593d8791942f1048f083d7b7b4e4
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-04-01 22:09:22 +0530

    Add files via upload

---
+++ atten.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int total_classes, attended_classes;
+    float percentage;
+
+    // Input
+    printf("Enter total number of classes: ");
+    scanf("%d", &total_classes);
+
+    printf("Enter number of classes attended: ");
+    scanf("%d", &attended_classes);
+
+    // Calculation
+    percentage = ((float)attended_classes / total_classes) * 100;
+
+    // Output
+    printf("\nAttendance Percentage = %.2f%%\n", percentage);
+
+    // Condition check
+    if (percentage >= 75) {
+        printf("Status: Allowed to sit in exam ✅\n");
+    } else {
+        printf("Status: Not allowed to sit in exam ❌\n");
+    }
+
+    return 0;
+}
